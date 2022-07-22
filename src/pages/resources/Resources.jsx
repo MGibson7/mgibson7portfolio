bb04cab918dbba65f1227d4b05f1cb274f5ecc9b
MGibson7 <MGibsonASC@gmail.com>
2022-07-22 12:33:42 -0400

    changed wording in resources

--- src/pages/resources/Resources.jsx
+++ src/pages/resources/Resources.jsx
@@ -3,7 +3,7 @@ import "./resources.css"
 const Resources = () => {
   return (
     <div className="resourceMain">
-      <h2>RESUME AND USEFUL LINKS</h2>
+      <h2>RESUME AND SOCIAL LINKS</h2>
       <div className="resumeLinks">
         <img src="./resume.png" alt="" />
         <img src="./linkedin.svg" alt="" />
