766836f8efe3113d025891649a812b25d549e9ec
MGibson7 <MGibsonASC@gmail.com>
2022-08-02 10:15:37 -0400

    updated testimonials

--- src/pages/testimonials/Testimonials.jsx
+++ src/pages/testimonials/Testimonials.jsx
@@ -50,16 +50,7 @@ const Testimonials = () => {
 </div>
 <div className="testimonialTests">
         <div className="testimonialOne">
-        <p>Needed an app to blast incoming project alerts out to my contractors in real time. 
-            Michael created it in less than 24 hours and showed me how to use the app easily on both my desktop and phone. 
-            Also was there to offer support or minor modications any time they were requested. 
-            Additionally, he performed data analysis on our customer base to reveal our top ROI in respect to ads and customer types.
-            His overall work offered a great boost to my start up in its early stages! 
-          </p>
-          <div className="person">
-            <img src="./junk.JPG" alt="" />
-          <h3> - ARNOLD FERREIRAS, CEO JUNK GURUS</h3>
-            </div>
+      
           
           
 
