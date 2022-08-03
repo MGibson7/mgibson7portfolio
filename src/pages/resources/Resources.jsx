71e6ae556c5f452befb995d5845af51249e0b2e0
MGibson7 <MGibsonASC@gmail.com>
2022-08-03 06:58:58 -0400

    added link to youtube icon to my yt page

--- src/pages/resources/Resources.jsx
+++ src/pages/resources/Resources.jsx
@@ -1,13 +1,25 @@
 import "./resources.css"
 
+
 const Resources = () => {
   return (
     <div className="resourceMain">
       <h2>RESUME AND SOCIAL LINKS</h2>
       <div className="resumeLinks">
+        <a href="https://www.youtube.com/channel/UCApFDEFeHM_Tr7Mf2LsGO3A/videos">
         <img src="./resume.png" alt="" />
+        </a>
+
+        <a href="https://linkedin.com">
         <img src="./linkedin.svg" alt="" />
+        </a>
+        
+        <a href="https://www.youtube.com/channel/UCApFDEFeHM_Tr7Mf2LsGO3A/videos" target = "_blank" rel="noreferrer noopener">
         <img src="./youtube.webp" alt="" />
+        </a>
+    
+        
+        
       </div>
 
       
