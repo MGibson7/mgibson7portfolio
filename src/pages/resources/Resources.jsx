148ccdf3e4bc3866e02ffb1f0788d74c6cfe0da2
MGibson7 <MGibsonASC@gmail.com>
2022-08-08 09:59:43 -0400

    updated resource links

--- src/pages/resources/Resources.jsx
+++ src/pages/resources/Resources.jsx
@@ -6,11 +6,11 @@ const Resources = () => {
     <div className="resourceMain">
       <h2>RESUME AND SOCIAL LINKS</h2>
       <div className="resumeLinks">
-        <a href="https://www.youtube.com/channel/UCApFDEFeHM_Tr7Mf2LsGO3A/videos" target = "_blank" rel="noreferrer noopener">
+        <a href="./MGibsonResume.pdf" target = "_blank" rel="noreferrer noopener">
         <img src="./resume.png" alt="" />
         </a>
 
-        <a href="https://linkedin.com" target = "_blank" rel="noreferrer noopener">
+        <a href="https://linkedin.com/in/michael-gibson7" target = "_blank" rel="noreferrer noopener">
         <img src="./linkedin.svg" alt="" />
         </a>
 
