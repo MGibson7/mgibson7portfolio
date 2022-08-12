7e24a546f434d61ea1f36bbe297446e28c5cc00d
MGibson7 <MGibsonASC@gmail.com>
2022-08-12 13:21:10 -0400

    added mouse over effect on projects

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -28,7 +28,8 @@ const Projects = () => {
 
     <h2>FEATURED PROJECTS</h2>
     <div className="iframes">
-        <div className="projectFea">
+        <div className="projectFea " onMouseOver={e => (e.currentTarget.style.border = "5px solid #7fffd4")} alt = ""
+    onMouseOut={e => (e.currentTarget.style.border = "none")}>
           <iframe scrolling="no" className="iframeImg" src="https://algoflix.herokuapp.com/" title="AlgoFlix"></iframe> 
         
         <a href="https://algoflix.herokuapp.com/" target = "_blank" rel="noreferrer noopener"><button>LIVE SITE</button></a>
@@ -38,7 +39,8 @@ const Projects = () => {
         <p className="descript" style={{display: display? 'flex' : 'none'}}>A full stack Netflix clone to host my algorithm videos. Utilizes MongoDB, Express, React, NodeJS, and SCSS </p>
         </div>
 
-        <div className="projectFea">
+        <div className="projectFea"  onMouseOver={e => (e.currentTarget.style.border = "5px solid #7fffd4")} alt = ""
+    onMouseOut={e => (e.currentTarget.style.border = "none")}>
         <iframe scrolling="no" className="iframeImg" src="https://ecommhalloween.vercel.app/" title="EcommHalloween"></iframe>
         <a href="https://ecommhalloween.vercel.app/" target = "_blank" rel="noreferrer noopener"><button>LIVE SITE</button></a>
         <a href="https://github.com/MGibson7/ecommhalloween" target = "_blank" rel="noreferrer noopener"><button className="source">SOURCE CODE</button></a>
@@ -46,7 +48,8 @@ const Projects = () => {
         <p className="descript" style={{display: display2? 'flex' : 'none'}}>A full stack Halloween E-Commerce Site complete with Admin page. Utilizes MongoDB, NextJS, React, and CSS </p>
         </div>
 
-        <div className="projectFea">
+        <div className="projectFea"  onMouseOver={e => (e.currentTarget.style.border = "5px solid #7fffd4")} alt = ""
+    onMouseOut={e => (e.currentTarget.style.border = "none")}>
         <iframe scrolling="no" className="iframeImg" src="https://fantasyfootballsocial2.herokuapp.com/" title="AlgoFlix"></iframe>
         <a href="https://fantasyfootballsocial2.herokuapp.com/" target = "_blank" rel="noreferrer noopener"><button>LIVE SITE</button></a>
         <a href="https://github.com/MGibson7/fantasyfootballsocial_final" target = "_blank" rel="noreferrer noopener"><button className="source">SOURCE CODE</button></a>
@@ -54,7 +57,8 @@ const Projects = () => {
         <p className="descript" style={{display: display3? 'flex' : 'none'}}>A full stack social media site. Utilizes MongoDB, Express, React, NodeJS, and CSS </p>
         </div> 
 
-        <div className="projectFea">
+        <div className="projectFea"  onMouseOver={e => (e.currentTarget.style.border = "5px solid #7fffd4")} alt = ""
+    onMouseOut={e => (e.currentTarget.style.border = "none")}>
         <iframe scrolling="no" className="iframeImg" src="https://mgibson7.github.io/networthStatement/" title="AlgoFlix"></iframe>
         <a href="https://mgibson7.github.io/networthStatement/" target = "_blank" rel="noreferrer noopener"><button>LIVE SITE</button></a>
         <a href="https://github.com/MGibson7/networthStatement" target = "_blank" rel="noreferrer noopener"><button className="source">SOURCE CODE</button></a>
@@ -62,7 +66,8 @@ const Projects = () => {
         <p className="descript" style={{display: display4? 'flex' : 'none'}}>A webapp for charting your networt and seeing where you stand. Utilizes vanilla Javascript and CSS  </p>
         </div> 
 
-        <div className="projectFea">
+        <div className="projectFea "  onMouseOver={e => (e.currentTarget.style.border = "5px solid #7fffd4")} alt = ""
+    onMouseOut={e => (e.currentTarget.style.border = "none")}>
         <iframe scrolling="no" className="iframeImg" src="https://mgibson7.github.io/freefiretools/" title="AlgoFlix"></iframe>
         <a href="https://mgibson7.github.io/freefiretools/" target = "_blank" rel="noreferrer noopener"><button>LIVE SITE</button></a>
         <a href="https://github.com/MGibson7/freefiretools" target = "_blank" rel="noreferrer noopener"><button className="source">SOURCE CODE</button></a>
